724fd7f0970ab03f902ca79a9fa78402c4a6928f
GerasYeah <57162490+GerasYeah@users.noreply.github.com>
2025-03-09 14:16:04 +0000

    Melhorias

--- xadrez.c
+++ xadrez.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-void moverPeca(int casas, char sentido[15]) {
+// Função recursiva para mover qualquer peça
+void moverPeca(int casas, char sentido[]) {
     if (casas > 0) {
         printf("%s\n", sentido);
         moverPeca(casas - 1, sentido);
     } 
 }
 
+// Função específica para mover o cavalo
 void moverCavalo() {
     int casas = 0;
 
+    // Loop aninhado para fazer o movimento "duplo" do cavalo
+    // Apenas para fins educacionais, existem maneiras melhores, obviamente.
     while (casas < 3) {
         if (casas < 2) {
             printf("Baixo\n");
@@ -23,18 +27,20 @@ void moverCavalo() {
 }
 
 int main() {
+    // Movimentando as peças uma por uma
     printf("Movimentação de Peças de Xadrez!\n");
 
     printf("\nMovimentação da Torre:\n");
     moverPeca(5, "Direita");
 
-    printf("\n\nMovimentação da Rainha:\n");
+    printf("\nMovimentação da Rainha:\n");
     moverPeca(8, "Esquerda");
 
-    printf("\n\nMovimentação do Cavalo:\n");
+    printf("\nMovimentação do Bispo:\n");
+    moverPeca(5, "Cima, Direita");
+
+    printf("\nMovimentação do Cavalo:\n");
     moverCavalo();
 
-    printf("\n\nMovimentação do Bispo:\n");
-    moverPeca(5, "Cima, Direita");
-    
+    return 0;
 }
