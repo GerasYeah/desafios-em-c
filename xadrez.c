d3d7980ca3a8113880758ecd00534c4cb8a76fd2
GerasYeah <57162490+GerasYeah@users.noreply.github.com>
2025-03-09 11:09:01 +0000

    Movimentos adicionados

--- xadrez.c
+++ xadrez.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
 
+void moverPeca(int casas, char sentido[15]) {
+    if (casas > 0) {
+        printf("%s\n", sentido);
+        moverPeca(casas - 1, sentido);
+    } 
+}
+
+void moverCavalo() {
+    int casas = 0;
+
+    while (casas < 3) {
+        if (casas < 2) {
+            printf("Baixo\n");
+        } else {
+            for (int i = 0; i < 1; i++) {
+                printf("Esquerda\n"); 
+            }
+        }
+        casas++;
+    }
+}
+
+int main() {
+    printf("Movimentação de Peças de Xadrez!\n");
+
+    printf("\nMovimentação da Torre:\n");
+    moverPeca(5, "Direita");
+
+    printf("\n\nMovimentação da Rainha:\n");
+    moverPeca(8, "Esquerda");
+
+    printf("\n\nMovimentação do Cavalo:\n");
+    moverCavalo();
+
+    printf("\n\nMovimentação do Bispo:\n");
+    moverPeca(5, "Cima, Direita");
+    
+}
